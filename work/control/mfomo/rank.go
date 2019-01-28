0d94f000968f47c9a76461ed3548127520903d64
heyuanlong <1023417614@qq.com>
2019-01-28 11:43:51 +0800

    xg

---
+++ work/control/mfomo/rank.go
@@ -0,0 +1,70 @@
+package mfomo
+
+import (
+	"net/http"
+	"strconv"
+
+	kinit "gorank/initialize"
+	kcode "gorank/work/code"
+	kbase "gorank/work/control/base"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Test struct {
+}
+
+func NewTest() *Test {
+	return &Test{}
+}
+func (ts *Test) Load() []kbase.RouteWrapStruct {
+	m := make([]kbase.RouteWrapStruct, 0)
+	m = append(m, kbase.Wrap("GET", "/mfomo/test1", ts.Test1, 0))
+	m = append(m, kbase.Wrap("GET", "/mfomo/test2", ts.Test2, 0))
+
+	return m
+}
+
+//-----------------------------------------------------------------------------------
+
+// http://127.0.0.1:8080/mfomo/test1?wallet_account=0x2d437Ec35E7d13a1AfF58250EeDc2808b92D9725&min_coins=0.5&chain_id=1
+type subtest2Struct struct {
+	WalletAccout string  `json:"wallet_account"` // 转币地址
+	MinCoins     float64 `json:"min_coins"`      //最少投入eth数量
+	ChainId      int     `json:"chain_id"`       //分红占比
+
+}
+
+func (ts *Test) Test1(c *gin.Context) {
+	callbackName := kbase.GetParam(c, "callback")
+	wallet_account := kbase.GetParam(c, "wallet_account")
+	kinit.LogWarn.Println("wallet_account:", wallet_account)
+
+	min_coins, err := strconv.ParseFloat(kbase.GetParam(c, "min_coins"), 0)
+	if err != nil {
+		kinit.LogWarn.Println(min_coins, err)
+		kbase.SendErrorJsonStr(c, kcode.PARAM_WRONG, callbackName)
+		return
+	}
+	chain_id, err := strconv.Atoi(kbase.GetParam(c, "chain_id"))
+	if err != nil {
+		kbase.SendErrorJsonStr(c, kcode.PARAM_WRONG, callbackName)
+		return
+	}
+
+	subObject := subtest2Struct{
+		WalletAccout: wallet_account,
+		MinCoins:     min_coins,
+		ChainId:      chain_id,
+	}
+	kbase.ReturnDataI(c, kcode.SUCCESS_STATUS, subObject, callbackName)
+}
+
+//-----------------------------------------------------------------------------------
+
+// http://127.0.0.1:8080/mfomo/test2
+func (ts *Test) Test2(c *gin.Context) {
+	c.Data(http.StatusOK, "text/plain", []byte("test2"))
+}
+
+//-----------------------------------------------------------------------------------
--- work/control/mfomo/test.go
+++ work/control/mfomo/test.go
@@ -11,34 +11,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Test struct {
+type Rank struct {
 }
 
-func NewTest() *Test {
-	return &Test{}
+func NewRank() *Rank {
+	return &Rank{}
 }
-func (ts *Test) Load() []kbase.RouteWrapStruct {
+func (ts *Rank) Load() []kbase.RouteWrapStruct {
 	m := make([]kbase.RouteWrapStruct, 0)
-	m = append(m, kbase.Wrap("GET", "/mfomo/test1", ts.Test1, 0))
-	m = append(m, kbase.Wrap("GET", "/mfomo/test2", ts.Test2, 0))
+	m = append(m, kbase.Wrap("GET", "/mfomo/rank1", ts.Rank1, 0))
+	m = append(m, kbase.Wrap("GET", "/mfomo/rank2", ts.Rank2, 0))
 
 	return m
 }
 
 //-----------------------------------------------------------------------------------
 
-// http://127.0.0.1:8080/mfomo/test1?wallet_account=0x2d437Ec35E7d13a1AfF58250EeDc2808b92D9725&min_coins=0.5&chain_id=1
-type subtest2Struct struct {
-	WalletAccout string  `json:"wallet_account"` // 转币地址
-	MinCoins     float64 `json:"min_coins"`      //最少投入eth数量
-	ChainId      int     `json:"chain_id"`       //分红占比
+// http://127.0.0.1:8080/mfomo/rank1?rank_id=1&min_coins=0.5&chain_id=1
+type subRank2Struct struct {
+	RankId   string  `json:"rank_id"`   // 转币地址
+	MinCoins float64 `json:"min_coins"` //最少投入eth数量
+	ChainId  int     `json:"chain_id"`  //分红占比
 
 }
 
-func (ts *Test) Test1(c *gin.Context) {
+func (ts *Rank) Rank1(c *gin.Context) {
 	callbackName := kbase.GetParam(c, "callback")
-	wallet_account := kbase.GetParam(c, "wallet_account")
-	kinit.LogWarn.Println("wallet_account:", wallet_account)
+	rank_id := kbase.GetParam(c, "rank_id")
 
 	min_coins, err := strconv.ParseFloat(kbase.GetParam(c, "min_coins"), 0)
 	if err != nil {
@@ -52,19 +51,19 @@ func (ts *Test) Test1(c *gin.Context) {
 		return
 	}
 
-	subObject := subtest2Struct{
-		WalletAccout: wallet_account,
-		MinCoins:     min_coins,
-		ChainId:      chain_id,
+	subObject := subRank2Struct{
+		RankId:   rank_id,
+		MinCoins: min_coins,
+		ChainId:  chain_id,
 	}
 	kbase.ReturnDataI(c, kcode.SUCCESS_STATUS, subObject, callbackName)
 }
 
 //-----------------------------------------------------------------------------------
 
-// http://127.0.0.1:8080/mfomo/test2
-func (ts *Test) Test2(c *gin.Context) {
-	c.Data(http.StatusOK, "text/plain", []byte("test2"))
+// http://127.0.0.1:8080/mfomo/rank2
+func (ts *Rank) Rank2(c *gin.Context) {
+	c.Data(http.StatusOK, "text/plain", []byte("Rank2"))
 }
 
 //-----------------------------------------------------------------------------------
